e0415033cb63a9e0e287022f288a3c03ee7716f1
even311379 <even311379@hotmail.com>
2019-10-05 20:21:21 +0800

    Update setup_app.py

--- setup_app.py
+++ setup_app.py
@@ -3,7 +3,8 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(
     external_stylesheets=[dbc.themes.CERULEAN,
-    'https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap'],
+    'https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap',
+    'https://freelancerlife.info/static/css/budai.css'],
     )
 app.title = '布袋鹽田生態調查資料'
 app.config.suppress_callback_exceptions = True
